1f7a5253914192c9912f36c63bb25b33a156b850
Cees van de Griend <cees@griend.eu>
2023-03-23 11:55:13 +0100

    docs: fixed logo moluk

--- bstb/logo.py
+++ bstb/logo.py
@@ -9,13 +9,13 @@ def moluk():
     # head
     d.append(draw.Circle(85, 50, 14, fill=COLOR))
     # body
-    d.append(draw.Line(85, 50, 85, 100, stroke=COLOR, stroke_width=14, fill="none", stroke_linecap="round"))
+    d.append(draw.Line(85, 50, 85, 100, stroke=COLOR, stroke_width=14, fill=COLOR, stroke_linecap="round"))
     # arms
-    d.append(draw.Line(40, 72, 132, 72, stroke=COLOR, stroke_width=14, fill="none", stroke_linecap="round"))
+    d.append(draw.Line(40, 72, 132, 72, stroke=COLOR, stroke_width=14, fill=COLOR, stroke_linecap="round"))
     # left leg
-    d.append(draw.Line(85, 100, 52, 127, stroke=COLOR, stroke_width=14, fill="none", stroke_linecap="round"))
+    d.append(draw.Line(85, 100, 52, 127, stroke=COLOR, stroke_width=14, fill=COLOR, stroke_linecap="round"))
     # right leg
-    d.append(draw.Line(85, 100, 119, 127, stroke=COLOR, stroke_width=14, fill="none", stroke_linecap="round"))
+    d.append(draw.Line(85, 100, 119, 127, stroke=COLOR, stroke_width=14, fill=COLOR, stroke_linecap="round"))
 
     d.set_render_size(150, 150)
     d.save_svg("moluk.svg")
